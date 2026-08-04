48a7c4a31ae23ff1ab8c04bfbcdd1f454d12668d
anomalin <anomalins@gmail.com>
2026-08-04 19:04:38 +0200

    updates

--- obj/Debug/net9.0/LocusAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/LocusAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LocusAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1308ae39209a4c1cd254e7cb7ed0f1f38b27b8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfe8d64afe293f1f4995c90984f2edcec1f4bb8")]
 [assembly: System.Reflection.AssemblyProductAttribute("LocusAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LocusAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
